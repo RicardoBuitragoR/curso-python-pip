28ea86aa597cccccc324584975a3a2733b1a6f54
RicardoBuitragoR <rrbuitragor@unal.edu.co>
2023-09-05 12:03:21 -0500

    modifique unas lineas en error del chart

--- app/charts.py
+++ app/charts.py
@@ -5,16 +5,15 @@ def generate_bar_chart(name, labels, values):
   ax.bar(labels, values)
   plt.savefig(f'./imgs/{name}.png')
   plt.close()
-
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
+  ax.pie(values, labels = labels)
   ax.axis('equal')
   plt.savefig('chart_pie.png')
   plt.close()
-
 if __name__ == '__main__':
-  labels = ['a', 'b', 'c']
-  values = [10, 40, 800]
-  # generate_bar_chart(labels, values)
+  labels = ['a', 'b','c']
+  values = [50,100,80]
+#  generate_bar_chat(labels, values)
   generate_pie_chart(labels, values)
+  
